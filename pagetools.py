2e51e8725a3972924d6a24b9aa96f7f86729dd82
dixond <dixond@acm.lviv.ua>
2013-01-04 00:25:58 +0200

    Refactored pagetools to a class; added faster method for splitting into sections.

--- pagetools.py
+++ pagetools.py
@@ -18,60 +18,111 @@
 
 import wikipedia as pywikibot
 import query
+import itertools
 
-def getSectionsFull(page, revId = None):
-    #get section titles and indexes
-    params = {
-        u'action' : u'parse',
-        u'prop'   : u'sections',
-        u'page'   : page.title()
-    }
-    if not revId is None:
-        params[u'oldid'] = revId
-    result = query.GetData(params, page.site())
-    sectionsDefinition = [item for item in result[u'parse'][u'sections'] if item[u'level'] == u'2']
+class PageRevision():
+    def __init__(self, page, revId = None, text = None):
+        self.page = page
+        self.revId = revId
+        self.text = text
 
-    # get section texts
-    sections = []
-    params = {
-        u'action'    : u'query',
-        u'titles'    : page.title(),
-        u'prop'      : u'revisions',
-        u'rvprop'    : u'content',
-        u'rvlimit'   : 1
-    }
-    if not revId is None:
-        params[u'rvstartid'] = revId
-        params[u'rvendid'] = revId
 
-    for sectionDefinition in sectionsDefinition:
-        if sectionDefinition[u'index'] is None:
-            continue
+    def getSectionDefinitions(self):
+        #get section titles and indexes
+        params = {
+            u'action': u'parse',
+            u'prop': u'sections',
+            u'page': self.page.title()
+        }
+        if not self.revId is None:
+            params[u'oldid'] = self.revId
+        result = query.GetData(params, self.page.site())
+        sectionsDefinition = [item for item in result[u'parse'][u'sections'] if item[u'level'] == u'2']
+        return sectionsDefinition
 
-        try:
-            sectionIndex = int(sectionDefinition[u'index'])
-        except ValueError:
-            continue
+    def getSectionsFull(self):
+        sectionsDefinition = self.getSectionDefinitions()
 
-        params[u'rvsection'] = sectionIndex
+        # get section texts
+        sections = []
+        params = {
+            u'action'    : u'query',
+            u'titles'    : self.page.title(),
+            u'prop'      : u'revisions',
+            u'rvprop'    : u'content',
+            u'rvlimit'   : 1
+        }
+        if not self.revId is None:
+            params[u'rvstartid'] = self.revId
+            params[u'rvendid'] = self.revId
 
-        result = query.GetData(params, page.site())
-
-        if u'error' in result:
-            errorCode = result[u'error'][u'code']
-            if errorCode == u'rvnosuchsection':
-                # skip this section
+        for sectionDefinition in sectionsDefinition:
+            if sectionDefinition[u'index'] is None:
                 continue
-            else:
-                # some other error
-                return []
 
-        sectionContent = result[u'query'][u'pages'].values()[0][u'revisions'][0][u'*']
+            try:
+                sectionIndex = int(sectionDefinition[u'index'])
+            except ValueError:
+                continue
 
-        sections.append({u'title':sectionDefinition[u'line'], u'text': sectionContent})
+            params[u'rvsection'] = sectionIndex
 
-    return sections
+            result = query.GetData(params, self.page.site())
 
-#page = pywikibot.Page(pywikibot.getSite(), u'Користувач:DixonD/sandbox')
-#sections = getSectionsFull(page)
+            if u'error' in result:
+                errorCode = result[u'error'][u'code']
+                if errorCode == u'rvnosuchsection':
+                    # skip this section
+                    continue
+                else:
+                    # some other error
+                    return []
+
+            sectionContent = result[u'query'][u'pages'].values()[0][u'revisions'][0][u'*']
+
+            sections.append({u'title':sectionDefinition[u'line'], u'text': sectionContent})
+
+        return sections
+
+    def getText(self):
+        # get text if not provided
+        if self.text is None:
+            params = {
+                u'action': u'query',
+                u'titles': self.page.title(),
+                u'prop': u'revisions',
+                u'rvprop': u'content',
+                u'rvlimit': 1
+            }
+            if not self.revId is None:
+                params[u'rvstartid'] = self.revId
+                params[u'rvendid'] = self.revId
+
+            result = query.GetData(params, self.page.site())
+            self.text = result[u'query'][u'pages'].values()[0][u'revisions'][0][u'*']
+
+        return self.text
+
+    def getSections(self):
+        # get text if not provided
+        if self.text is None:
+            self.getText()
+
+        # get sections definitions
+        sectionDefinitions = self.getSectionDefinitions()
+
+        # check if response is proper; see https://bugzilla.wikimedia.org/show_bug.cgi?id=25203
+        for sectionInfo in sectionDefinitions:
+            if sectionInfo[u'byteoffset'] >= len(self.text):
+                # we need to go with slow but correct way
+                return self.getSectionsFull()
+
+        #split text to sections
+        sections = [{u'title':pair[0][u'line'], u'text': self.text[pair[0][u'byteoffset']:pair[1][u'byteoffset']].strip()} for pair in itertools.izip(sectionDefinitions, sectionDefinitions[1:] + [{u'byteoffset': len(self.text)}])]
+
+        return sections
+
+#page = pywikibot.Page(pywikibot.getSite(), u'Вікіпедія:Кнайпа (політики)')
+#pageRevision = PageRevision(page)
+#sections = pageRevision.getSections()
 #print sections
